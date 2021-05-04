a724350b98f29008a700bba393a9f11ea1f288d5
Jéssica Bernardi Petersen <jessica041ster@gmail.com>
2021-05-03 21:34:04 -0300

    app

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # PHP com PDO e MySQL
- PHP com PDO e MySQL
+
+CRUD básico em PHP com PDO e MySQL.
+
+
+**Estrutura**
+-banco de dados (sql)
+-app_lista_tarefas_public (fica dentro do htdocs no xampp)
+-app_lista_tarefa (fica dentro da pasta do xampp - privado)
+
---
+++ banco_de_dados.sql
@@ -0,0 +1,22 @@
+http://localhost/phpmyadmin/
+
+Definições gerais do Database: 
+	php_com_pdo
+	utf8mb4_unicode_ci
+	
+	
+create table tb_status(
+	id int not null primary key auto_increment,
+    status varchar(25) not null
+);
+
+insert into tb_status(status)values('pendente');
+insert into tb_status(status)values('realizado');
+
+create table tb_tarefas(
+	id int not null primary key auto_increment,
+    id_status int not null default 1,
+    foreign key(id_status) references tb_status(id),
+	tarefa text not null,
+    data_cadastrado datetime not null default current_timestamp
+)
